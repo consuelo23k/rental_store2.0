b65c1a6671a4b0a9fcd49a08eea0f02b8c9e54bb
Consuelo <consuelosilva100k@gmail.com>
2024-10-18 14:42:10 -0300

    criar um checkbox para indicar que o filme foi assistido

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
-
 import { FaStar } from "react-icons/fa";
 
 const imagesURL = import.meta.env.VITE_IMG;
 
-const MovieCard = ({ movie, showLink = true }) => {
+const MovieCard = ({ movie, showLink = true, handleWatchedChange }) => {
   return (
     <div className="movie_card">
       <img src={imagesURL + movie.poster_path} alt={movie.title} />
@@ -12,6 +11,15 @@ const MovieCard = ({ movie, showLink = true }) => {
       <p>
         <FaStar /> {movie.vote_average}
       </p>
+      {/* Checkbox para marcar como assistido */}
+      <label>
+        <input
+          type="checkbox"
+          checked={movie.watched}
+          onChange={() => handleWatchedChange(movie.id)}
+        />
+        Marcar como assistido
+      </label>
       {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
   );
