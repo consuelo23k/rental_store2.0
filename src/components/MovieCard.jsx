3f5beadedb80c12d09a8487f6431e99adee1af4e
Consuelo <consuelosilva100k@gmail.com>
2024-10-30 16:56:23 -0300

    mudanças front

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
+import axios from "axios";
 
 const imagesURL = import.meta.env.VITE_IMG;
 
-const MovieCard = ({ movie, showLink = true, handleWatchedChange }) => {
+const MovieCard = ({ movie, showLink = true }) => {
+  const [watched, setWatched] = useState(movie.watched);
+
+  const handleWatchedChange = async (movie_id) => {};
+
   return (
     <div className="movie_card">
       <img src={imagesURL + movie.poster_path} alt={movie.title} />
@@ -11,11 +17,10 @@ const MovieCard = ({ movie, showLink = true, handleWatchedChange }) => {
       <p>
         <FaStar /> {movie.vote_average}
       </p>
-      {/* Checkbox para marcar como assistido */}
       <label>
         <input
           type="checkbox"
-          checked={movie.watched}
+          checked={watched}
           onChange={() => handleWatchedChange(movie.id)}
         />
         Marcar como assistido
