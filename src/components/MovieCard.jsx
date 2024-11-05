cb7d88bcadaabeb172736d14c82250be74eab4ba
Consuelo <consuelosilva100k@gmail.com>
2024-11-05 15:07:13 -0300

    garantir que o check box vai fucionar corretamente

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Await, Link } from "react-router-dom";
 import { FaStar } from "react-icons/fa";
 import axios from "axios";
 
@@ -8,7 +8,19 @@ const imagesURL = import.meta.env.VITE_IMG;
 const MovieCard = ({ movie, showLink = true }) => {
   const [watched, setWatched] = useState(movie.watched);
 
-  const handleWatchedChange = async (movie_id) => {};
+  const handleWatchedChange = async (movie_id) => {
+    if (watched === false) {
+      await axios.post(
+        `http://localhost:3000/filmeAssistido?movieId=${movie_id}`
+      );
+      setWatched(true);
+    } else {
+      await axios.delete(
+        `http://localhost:3000/filmeNaoAssistido?movieId=${movie_id}`
+      );
+      setWatched(false);
+    }
+  };
 
   return (
     <div className="movie_card">
