813006fa19307c0ff95a0ae3f22dfe21077ca00f
Consuelo <consuelosilva100k@gmail.com>
2024-11-11 14:36:49 -0300

    botão para marcar ou desmarcar o filme na lista de desejos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
-import { Await, Link } from "react-router-dom";
-import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaStar, FaRegStar } from "react-icons/fa";
 import axios from "axios";
 
 const imagesURL = import.meta.env.VITE_IMG;
 
 const MovieCard = ({ movie, showLink = true }) => {
   const [watched, setWatched] = useState(movie.watched);
+  const [isInWishlist, setIsInWishlist] = useState(movie.isInWishlist || false);
 
   const handleWatchedChange = async (movie_id) => {
     if (watched === false) {
@@ -19,6 +20,16 @@ const MovieCard = ({ movie, showLink = true }) => {
         `http://localhost:3000/filmeNaoAssistido?movieId=${movie_id}`
       );
       setWatched(false);
+    }
+  };
+  const toggleWishlist = async (movie_id) => {
+    if (isInWishlist) {
+      await axios.delete(
+        `http://localhost:3000/removeWishlist?movieId=${movie_id}`
+      );
+      setIsInWishlist(false);
+    } else {
+      await axios.post(`http://localhost:3000/addWishlist?movieId=${movie_id}`);
     }
   };
 
@@ -37,6 +48,16 @@ const MovieCard = ({ movie, showLink = true }) => {
         />
         Marcar como assistido
       </label>
+      <button
+        onClick={() => toggleWishlist(movie.id)}
+        style={{ background: "none", border: "none", cursor: "pointer" }}
+      >
+        {isInWishlist ? (
+          <FaRegStar color="gold" size={24} />
+        ) : (
+          <FaRegStar color="gold" size={24} />
+        )}
+      </button>
       {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
   );
